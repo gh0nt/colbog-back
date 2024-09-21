1aa11d20f3e56e3cebc30817772cfe8b9d2078f3
Ghont <giohanpuentes@gmail.com>
2024-09-21 11:48:37 -0500

    first commit

---
+++ src/main/java/com/colbog/colbog/ColbogApplication.java
@@ -0,0 +1,13 @@
+package com.colbog.colbog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColbogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColbogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/colbog/colbog/ColbogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colbog.colbog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColbogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
